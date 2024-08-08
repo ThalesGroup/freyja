4b09a4452d88f1b209bd94eb542000445ad8c78a
Louis Cailliot <louis.cailliot@thalesgroup.com>
2024-08-08 17:27:19 +0200

    dev: machine start implemented

--- internal/shellcli/machineStart.go
+++ internal/shellcli/machineStart.go
@@ -2,8 +2,12 @@ package shellcli
 
 import (
 	"github.com/spf13/cobra"
-	"log"
+	"go.uber.org/zap"
+	"os"
+	"strings"
 )
+
+var domainName string
 
 // commands definitions
 var machineStartCmd = &cobra.Command{
@@ -13,6 +17,32 @@ var machineStartCmd = &cobra.Command{
 	TraverseChildren: true, // ensure local flags do not spread to sub commands
 
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Start machine")
+		// logger
+		setLogger()
+
+		domain, _ := LibvirtConnexion.DomainLookupByName(domainName)
+		err := LibvirtConnexion.DomainCreate(domain)
+		if err != nil {
+			if strings.Contains(err.Error(), "already running") {
+				Logger.Warn("Skip : machine is already running",
+					zap.String("machine", domainName))
+				os.Exit(0)
+			} else if strings.Contains(err.Error(), "not found") {
+				Logger.Error("Machine not found",
+					zap.String("machine", domainName))
+				os.Exit(1)
+			} else {
+				Logger.Panic("Could not start the domain",
+					zap.String("machine", domain.Name),
+					zap.Error(err))
+				os.Exit(1)
+			}
+		}
+		Logger.Info("Starting domain",
+			zap.String("domain", domainName))
 	},
+}
+
+func init() {
+	machineStartCmd.Flags().StringVarP(&domainName, "name", "n", "", "Name of the machine to start.")
 }
